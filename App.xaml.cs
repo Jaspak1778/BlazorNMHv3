30caeb53f2e5e22d0336cbbd9b737f5dac93a445
Jani Pekkala <pekkala.jani@hotmail.fi>
2024-11-14 12:29:24 +0200

    lisä testejä ja alustusta

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,6 +6,22 @@
         {
             InitializeComponent();
 
+            // Määritellään tapahtumankäsittelijä käsittelemättömille poikkeuksille
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                var exception = args.ExceptionObject as Exception;
+                if (exception != null)
+                {
+                    // Kirjaa virhe konsoliin (tai halutessa raportoi palvelimelle)
+                    Console.WriteLine($"Unhandled exception: {exception.Message}");
+                    Console.WriteLine(exception.StackTrace);
+                }
+                else
+                {
+                    Console.WriteLine("Unhandled non-exception error occurred.");
+                }
+            };
+
             MainPage = new MainPage();
         }
     }
